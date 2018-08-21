e74f6c7f035b7b02acf464bdc4621315cbc1b4ca
Tomas Cohen Arazi <tomascohen@theke.io>
2018-08-21 15:57:48 -0300

    Issue #1: Add initial code
    Borrowed code from KitchenSink
    
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ Koha/Plugin/Com/Theke/BiblioCommons.pm
@@ -0,0 +1,68 @@
+package Koha::Plugin::Com::Theke::BiblioCommons;
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This program comes with ABSOLUTELY NO WARRANTY;
+
+use Modern::Perl;
+
+use base qw(Koha::Plugins::Base);
+
+use Mojo::JSON qw(decode_json);;
+
+our $VERSION = "{VERSION}";
+
+our $metadata = {
+    name            => 'BiblbioCommons connector plugin for Koha',
+    author          => 'Theke Solutions',
+    date_authored   => '2018-08-23',
+    date_updated    => "2018-08-23",
+    minimum_version => '18.05.00.000',
+    maximum_version => undef,
+    version         => $VERSION,
+    description     => 'This plugin implements API endpoints required'
+                     . 'for the integration of BiblioCommons with Koha.'
+};
+
+sub new {
+    my ( $class, $args ) = @_;
+
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+sub api_routes {
+    my ( $self, $args ) = @_;
+
+    my $spec_str = $self->mbf_read('openapi.json');
+    my $spec     = decode_json($spec_str);
+
+    return $spec;
+}
+
+sub api_namespace {
+    my ( $self ) = @_;
+    
+    return 'bibliocommons';
+}
+
+1;
