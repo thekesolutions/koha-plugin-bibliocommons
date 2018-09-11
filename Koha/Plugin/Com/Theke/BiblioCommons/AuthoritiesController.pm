827cee3f1b831395351e46b93ee0a7d996eb54ca
Tomas Cohen Arazi <tomascohen@theke.io>
2018-09-11 15:57:26 -0700

    Issue #16: Add /authorities/:authority_id and /authorities/ids
    Signed-off-by: Tomas Cohen Arazi <tomascohen@theke.io>

---
+++ Koha/Plugin/Com/Theke/BiblioCommons/AuthoritiesController.pm
@@ -0,0 +1,120 @@
+package Koha::Plugin::Com::Theke::BiblioCommons::AuthoritiesController;
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This program comes with ABSOLUTELY NO WARRANTY;
+
+use Modern::Perl;
+
+use Mojo::Base 'Mojolicious::Controller';
+
+use C4::AuthoritiesMarc;
+use MARC::Record::MiJ;
+
+=head1 Koha::Plugin::Com::Theke::BiblioCommons::AuthoritiesController
+
+A class implementing the controller methods for the authorities-related API
+
+=head2 Class Methods
+
+=head3 list_ids
+
+Method that lists authorities ids based on the filters
+
+=cut
+
+sub list_ids {
+    my $c = shift->openapi->valid_input or return;
+
+    my $args = $c->validation->output;
+    my $attributes = {};
+    my ( $params, $reserved_params ) = $c->extract_reserved_params($args);
+
+    # Merge pagination into query attributes
+    $c->dbic_merge_pagination({ filter => $attributes, params => $reserved_params });
+
+    my $timestamp_gte = $params->{'timestamp[gte]'};
+    my $timestamp_lte = $params->{'timestamp[lte]'};
+
+    my $filter;
+
+    if ( $timestamp_gte or $timestamp_lte ) {
+
+        # add a filter!
+        my @filter_conditions;
+        push @filter_conditions, { '>=' => $timestamp_gte }
+            if $timestamp_gte;
+        push @filter_conditions, { '<=' => $timestamp_lte }
+            if $timestamp_lte;
+
+        $filter->{ 'modification_time' } = \@filter_conditions;
+    }
+
+    my @ids = map { _to_id_object($_) }
+        Koha::Authorities->search( $filter, $attributes )->get_column('authid');
+
+    return $c->render( status => 200, openapi => \@ids );
+}
+
+=head3 get_authority
+
+=cut
+
+sub get_authority {
+    my $c = shift->openapi->valid_input or return;
+
+    my $authority_id = $c->validation->param('authority_id');
+    my $record       = C4::AuthoritiesMarc::GetAuthority( $authority_id );
+
+    unless ( $record ) {
+        return $c->render( status => 404, openapi => { error => "Object not found." } );
+    }
+
+    $c->app->types->type( marcxml => 'application/marcxml+xml' );
+    $c->app->types->type( mij     => 'application/marc-in-json' );
+    $c->app->types->type( marc    => 'application/marc' );
+
+    return
+        $c->respond_to(
+            marcxml => { status => 200, format => 'marcxml', text => $record->as_xml_record },
+            mij     => { status => 200, format => 'mij',     text => $record->to_mij  },
+            marc    => { status => 200, format => 'marc',    text => $record->as_usmarc },
+            any     => { status => 200, format => 'marcxml', text => $record->as_xml_record }
+        );
+}
+
+=head2 Internal methods
+
+=head3 to_id_object
+
+Helper class to wrap an authority record id in a HAL-like structure
+
+=cut
+
+sub _to_id_object {
+    my ($authority_id) = @_;
+
+    my $id_object = {
+        id     => $authority_id,
+        _links => {
+            self => {
+                href => '/api/v1/contrib/bibliocommons/authorities/' . $authority_id
+            }
+        }
+    };
+
+    return $id_object;
+}
+
+1;
--- README.md
+++ README.md
@@ -9,19 +9,21 @@ WHOLE POINT OF THIS!
 
 ## Implemented endpoints
 
-* /biblios/:biblio_id
-* /biblios/:biblio_id/holds/count
-* /biblios/:biblio_id/items
+* /authorities/_:authority_id_
+* /authorities/ids
+* /biblios/_:biblio_id_
+* /biblios/_:biblio_id_/holds/count
+* /biblios/_:biblio_id_/items
 * /biblios/ids
-* /items/:item_id
-* /items/:item_id/holds/count
+* /items/_:item_id_
+* /items/_:item_id_/holds/count
 * /items/ids
 * /libraries
 
 ## Install
 
 Download the latest _.kpz_ file from the [releases](https://github.com/thekesolutions/koha-plugin-bibliocommons/releases) page.
-Install it as any other plugin following the general [plugin install instrucions](https://wiki.koha-community.org/wiki/Koha_plugins).
+Install it as any other plugin following the general [plugin install instructions](https://wiki.koha-community.org/wiki/Koha_plugins).
 
 Caveat: If you want to try this endpoints, you will need to do it on top of the [Koha master branch](https://gitlab.com/koha-community/Koha)
 adding the patches from [bug 21116](https://bugs.koha-community.org/bugzilla3/show_bug.cgi?id=21116).
